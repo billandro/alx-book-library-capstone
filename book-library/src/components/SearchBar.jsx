29cdb796022a6f25cdcf83f6ddf7785a62b7f3ff
Bill Qubeka <Bill Qubeka>
2024-10-20 21:58:25 +0200

    Fix the small searchbar on biggers screen

--- book-library/src/components/SearchBar.jsx
+++ book-library/src/components/SearchBar.jsx
@@ -19,7 +19,7 @@ const SearchBar = ({ placeholder, onSearch }) => {
                 value={query}
                 onChange={handleChange}
                 placeholder={placeholder}
-                className="w-16 sm:w-0 flex-grow px-3 py-2 focus:outline-none"
+                className="w-16 sm:w-auto flex-grow px-3 py-2 focus:outline-none"
             />
             <button type="submit" className="w-auto ml-2 bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-500">
                 Search
